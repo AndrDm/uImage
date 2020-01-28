55f39f3ae7e5815d58b485efddedb80ae8bfe618
AndrDm <58330296+AndrDm@users.noreply.github.com>
2020-01-28 22:52:52 +0100

    Image and Zoom handling moved from Viewer to the µImage.Display (Custom Control)

---
+++ Source/µImage.Display/uImageControl.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace µImage.Display
+{
+   
+    [TemplatePart(Name = "µImage", Type = typeof(Image))]
+    public class uImageControl : Control
+    {
+        public static Image _µimage;
+
+        static uImageControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(uImageControl), new FrameworkPropertyMetadata(typeof(uImageControl)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _µimage = GetTemplateChild("µImage") as Image;
+
+            if (null == _µimage) throw new NullReferenceException("Template Part µImage is not available");
+            else{
+            _µimage.LayoutTransform = new ScaleTransform();
+            _µimage.MouseWheel += OnDisplayControlMouseWheel;
+        }
+    }
+
+    private void OnDisplayControlMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+
+        ScaleTransform obj = (ScaleTransform)_µimage.LayoutTransform;
+
+        double zoom = e.Delta > 0 ? .1 : -.1;
+        obj.ScaleY = obj.ScaleX = (obj.ScaleX += zoom).LimitToRange(.1, 10);
+
+        BitmapScalingMode mode = obj.ScaleX > 5 ? BitmapScalingMode.HighQuality : BitmapScalingMode.NearestNeighbor;
+        RenderOptions.SetBitmapScalingMode(_µimage, mode);
+
+        e.Handled = true;
+    }
+}
+
+    public static class InputExtensions
+    {
+        public static double LimitToRange(this double value, double inclusiveMinimum, double inclusiveMaximum)
+        {
+            if (value < inclusiveMinimum) { return inclusiveMinimum; }
+            if (value > inclusiveMaximum) { return inclusiveMaximum; }
+            return value;
+        }
+    }
+}
--- Source/µImage.Viewer/MainWindow.xaml.cs
+++ Source/µImage.Viewer/MainWindow.xaml.cs
@@ -12,33 +12,7 @@ namespace µImage.Viewer
         public MainWindow()
         {
             InitializeComponent();
-
-            µImage.LayoutTransform = (ScaleTransform) (new ScaleTransform());
-            µImage.MouseWheel += image_MouseWheel;
         }
-
-
-        private void image_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            ScaleTransform obj = (ScaleTransform)µImage.LayoutTransform;
-
-            double zoom = e.Delta > 0 ? .1 : -.1;
-            obj.ScaleY = obj.ScaleX = (obj.ScaleX += zoom).LimitToRange(.1, 10);
-
-            BitmapScalingMode mode = obj.ScaleX > 5 ? BitmapScalingMode.HighQuality : BitmapScalingMode.NearestNeighbor;
-            RenderOptions.SetBitmapScalingMode(µImage, mode);
-            Title = "µImage.Viewer - Zoom = " + obj.ScaleX;
-        }
-
-    }
-
-    public static class InputExtensions
-    {
-        public static double LimitToRange(this double value, double inclusiveMinimum, double inclusiveMaximum)
-        {
-            if (value < inclusiveMinimum) { return inclusiveMinimum; }
-            if (value > inclusiveMaximum) { return inclusiveMaximum; }
-            return value;
-        }
+	//Mouse Wheel handling moved to the control level
     }
 }
