2ddc361595bfb4296a2e4330e6fb49a950480aef
AndrDm <58330296+AndrDm@users.noreply.github.com>
2020-01-30 22:36:30 +0100

    Tabs replaced to spaces (cosmetic)

--- Source/µImage.Display/Magnification.cs
+++ Source/µImage.Display/Magnification.cs
@@ -6,13 +6,13 @@ using System.Windows.Media;
 
 namespace µImage.Display
 {
-	public partial class uImageControl : Control
-	{
+    public partial class uImageControl : Control
+    {
         public double Magnification
-		{
-			get { return (double)GetValue(MagnificationProperty); }
-			set { SetValue(MagnificationProperty, value); }
-		}
+        {
+            get { return (double)GetValue(MagnificationProperty); }
+            set { SetValue(MagnificationProperty, value); }
+        }
 
         public Point MousePosition
         {
@@ -21,27 +21,27 @@ namespace µImage.Display
         }
 
         
-		private static void OnMagnificationChanged(DependencyObject image, DependencyPropertyChangedEventArgs magnification)
-		{
-			ApplyMagnification((uImageControl)image, (double)magnification.NewValue);
-		}
+        private static void OnMagnificationChanged(DependencyObject image, DependencyPropertyChangedEventArgs magnification)
+        {
+            ApplyMagnification((uImageControl)image, (double)magnification.NewValue);
+        }
 
         private static void ApplyMagnification(uImageControl image, double magnificationValue)
-		{
-			if (image.part_µImage != null){
-				ScaleTransform obj = (ScaleTransform)image.part_µImage.LayoutTransform;
-				obj.ScaleX = obj.ScaleY = magnificationValue;
-				RenderOptions.SetBitmapScalingMode(image.part_µImage, BitmapScalingMode.HighQuality); //ToDo: depends on zoom or external settings
-				image.part_µZoom.Text = $"{magnificationValue*100:N0}%";
-			}
-		}
+        {
+            if (image.part_µImage != null){
+                ScaleTransform obj = (ScaleTransform)image.part_µImage.LayoutTransform;
+                obj.ScaleX = obj.ScaleY = magnificationValue;
+                RenderOptions.SetBitmapScalingMode(image.part_µImage, BitmapScalingMode.HighQuality); //ToDo: depends on zoom or external settings
+                image.part_µZoom.Text = $"{magnificationValue*100:N0}%";
+            }
+        }
 
-		private void CenterViewerAroundMouse(Point center)
-		{
-			if (part_µScrollViewer != null){
-				part_µScrollViewer.ScrollToHorizontalOffset(center.X * Magnification - Mouse.GetPosition(part_µScrollViewer).X);
-				part_µScrollViewer.ScrollToVerticalOffset(center.Y * Magnification - Mouse.GetPosition(part_µScrollViewer).Y);
-			}
-		}
+        private void CenterViewerAroundMouse(Point center)
+        {
+            if (part_µScrollViewer != null){
+                part_µScrollViewer.ScrollToHorizontalOffset(center.X * Magnification - Mouse.GetPosition(part_µScrollViewer).X);
+                part_µScrollViewer.ScrollToVerticalOffset(center.Y * Magnification - Mouse.GetPosition(part_µScrollViewer).Y);
+                }
+                }
     }
 }
