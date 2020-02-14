300f3781b5393dbb968804b6eb7d13be2fed5eae
Andrey Dmitriev <58330296+AndrDm@users.noreply.github.com>
2020-02-14 09:16:27 +0100

    µCore added
    at this moment just debug output

--- Source/µImage.Core/µCore.cs
+++ Source/µImage.Core/µCore.cs
@@ -1,20 +1,6 @@
 using System;
 using System.Windows;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Diagnostics;
-using System.Collections;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
-using System.Security;
-using System.Security.Permissions;
-using System.Globalization;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace µ.Core 
 {
